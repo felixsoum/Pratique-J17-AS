8679e99d759ddeac4f8086d4d3d24639c22c6cfa
felixsoum <felix@felixsoum.com>
2023-09-19 10:06:25 -0400

    Ajout outil météo

---
+++ MonOutil/Meteo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonOutil
+{
+    internal class Meteo
+    {
+        public Meteo()
+        {
+            double tempEnCelsius;
+            double tempEnFahrenheit;
+            string? line;
+
+            Console.WriteLine("Bienvenue à l'outil Météo.");
+            Console.WriteLine("\n\nEntrez la température en Celcius:\n\n");
+            line = Console.ReadLine();
+            tempEnCelsius = Convert.ToDouble(line);
+            tempEnFahrenheit = (tempEnCelsius * 9 / 5) + 32;
+            Console.WriteLine($"La température en Fahrenheit: {tempEnFahrenheit} ");
+        }
+    }
+}
--- MonOutil/Program.cs
+++ MonOutil/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using MonOutil;
+
+Console.Write("Bonjour, quel est votre nom?");
+
+string? username;
+username = Console.ReadLine();
+
+// Code qui accueille l'utilisateur par son nom
+Console.WriteLine($"Bienvenue {username} à MonOutil.");
+
+new Meteo();
